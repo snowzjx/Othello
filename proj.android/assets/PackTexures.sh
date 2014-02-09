4e69a0d275afec9eb953b588650a24611fbcb47d
Snow <snow.zjx@gmail.com>
2014-02-09 14:30:21 +0800

    Add project configuration file.

---
+++ proj.android/assets/PackTexures.sh
@@ -0,0 +1,101 @@
+#!/bin/sh
+
+#  PackTexures.sh
+#  Othello
+#
+#  Created by Snow on 1/28/14.
+#
+
+TP="/usr/local/bin/TexturePacker"
+if [ "${ACTION}" = "clean" ]
+
+then
+echo "cleaning..."
+rm ./1536p/*.*
+rm ./768p/*.*
+rm ./320p/*.*
+
+else
+echo "building..."
+
+${TP} --smart-update \
+--format cocos2d \
+--data 1536p/othelloscene.plist \
+--sheet 1536p/othelloscene.pvr.ccz \
+--dither-fs-alpha \
+--opt RGBA4444 \
+../OthelloArt/OthelloScene/*.png
+
+${TP} --smart-update \
+--format cocos2d \
+--data 768p/othelloscene.plist \
+--sheet 768p/othelloscene.pvr.ccz \
+--dither-fs-alpha \
+--scale 0.5 \
+--opt RGBA4444 \
+../OthelloArt/OthelloScene/*.png
+
+${TP} --smart-update \
+--format cocos2d \
+--data 320p/othelloscene.plist \
+--sheet 320p/othelloscene.pvr.ccz \
+--dither-fs-alpha \
+--scale 0.25 \
+--opt RGBA4444 \
+../OthelloArt/OthelloScene/*.png
+
+${TP} --smart-update \
+--format cocos2d \
+--data 1536p/menuscene.plist \
+--sheet 1536p/menuscene.pvr.ccz \
+--dither-fs-alpha \
+--opt RGBA4444 \
+../OthelloArt/MenuScene/*.png
+
+${TP} --smart-update \
+--format cocos2d \
+--data 768p/menuscene.plist \
+--sheet 768p/menuscene.pvr.ccz \
+--dither-fs-alpha \
+--scale 0.5 \
+--opt RGBA4444 \
+../OthelloArt/MenuScene/*.png
+
+${TP} --smart-update \
+--format cocos2d \
+--data 320p/menuscene.plist \
+--sheet 320p/menuscene.pvr.ccz \
+--dither-fs-alpha \
+--scale 0.25 \
+--opt RGBA4444 \
+../OthelloArt/MenuScene/*.png
+
+${TP} --smart-update \
+--format cocos2d \
+--data 1536p/finishscene.plist \
+--sheet 1536p/finishscene.pvr.ccz \
+--dither-fs-alpha \
+--opt RGBA4444 \
+../OthelloArt/FinishScene/*.png
+
+${TP} --smart-update \
+--format cocos2d \
+--data 768p/finishscene.plist \
+--sheet 768p/finishscene.pvr.ccz \
+--dither-fs-alpha \
+--scale 0.5 \
+--opt RGBA4444 \
+../OthelloArt/FinishScene/*.png
+
+${TP} --smart-update \
+--format cocos2d \
+--data 320p/finishscene.plist \
+--sheet 320p/finishscene.pvr.ccz \
+--dither-fs-alpha \
+--scale 0.25 \
+--opt RGBA4444 \
+../OthelloArt/FinishScene/*.png
+
+
+fi
+exit 0
---
+++ proj.android/gen/com/whitemirror/othello/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.whitemirror.othello;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
